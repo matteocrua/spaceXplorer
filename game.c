a612a76beb6a63868aea03f1990fda2a9821c54d
matteocrua <36966567+matteocrua@users.noreply.github.com>
2025-04-14 11:08:15 +0100

    Stage 3 – junk and asteroid spawning

--- game.c
+++ game.c
@@ -4,13 +4,15 @@ Filename: game.c
     Author: Matteo Crua
     Date: 11/04/2025
     Input/Output:
-    Version 1.1
+    Version 2.0
     Log:
       1.0: initial, added map initialisation and printMap() 11/04/2025
       1.1: added comments 11/04/2025
+      2.0: added junk initialisation 11/04/2025
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "game.h"
 
 // define the map and its size
@@ -27,6 +29,45 @@ void initMap(){
                 map[i][j] = BORDER;
             }
         }
+    }
+}
+
+void initJunk(junk arrJunk[], int numJunk) {
+    // loop through the given number of junk
+    for (int i = 0; i < numJunk; i++) {
+        // generate random positions avoiding borders
+        arrJunk[i].pos.x = 1 + rand() % (MAPSIZE - 2);
+        arrJunk[i].pos.y = 1 + rand() % (MAPSIZE - 2);
+
+        // 1 in 5 chance to be super junk
+        arrJunk[i].isSuperJunk = (rand() % 10 == 0);
+
+        // set value based on junk type
+        // if super junk, value is 50, else 10
+        arrJunk[i].value = arrJunk[i].isSuperJunk ? 20 : 10;
+
+        // place on map with corresponding symbol
+        map[arrJunk[i].pos.y][arrJunk[i].pos.x] = arrJunk[i].isSuperJunk ? 'J' : 'j';
+    }
+}
+
+// function to initialise the asteroids
+void initAsteroid(asteroid arrAsteroid[], int numAsteroid) {
+    // loop through the given number of asteroids
+    for (int i = 0; i < numAsteroid; i++) {
+        // generate random positions avoiding borders
+        arrAsteroid[i].pos.x = 1 + rand() % (MAPSIZE - 2);
+        arrAsteroid[i].pos.y = 1 + rand() % (MAPSIZE - 2);
+
+        // 1 in 5 chance to be super asteroid
+        arrAsteroid[i].isSuperAsteroid = (rand() % 5 == 0);
+
+        // set damage based on asteroid type
+        // if super asteroid, damage is 20, else 10
+        arrAsteroid[i].dmg = arrAsteroid[i].isSuperAsteroid ? 20 : 10;
+
+        // place on map with corresponding symbol
+        map[arrAsteroid[i].pos.y][arrAsteroid[i].pos.x] = arrAsteroid[i].isSuperAsteroid ? 'A' : 'a';
     }
 }
 
--- spaceXplorer.c
+++ spaceXplorer.c
@@ -4,13 +4,36 @@ Filename: spaceXplorer.c
     Author: Matteo Crua
     Date: 11/04/2025
     Input/Output: N/A
-    Version 1.0
+    Version 2.1
     Log:
       1.0: initial added initMap() and printMap() 11/04/2025
+      2.0: added initJunk() and initAsteroid() 11/04/2025
+      2.1: added comments 11/04/2025
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "game.h"
+
+// define the number of starting junk and asteroids
+int asteroidCount = 10;
+int junkCount = 10;
+
 int main() {
+    // seed the random number generator
+    srand(time(NULL));
+
+    // declare the objects
+    junk arrJunk[junkCount];
+    asteroid arrAsteroid[asteroidCount];
+
+    // initialise the objects
     initMap();
+    initAsteroid(arrAsteroid, asteroidCount);
+    initJunk(arrJunk, junkCount);
+
+    // print the map
     printMap();
 }
 
